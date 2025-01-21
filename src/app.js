4935ffaca0b6836788ac850f6e8251aab9a4ce40
dev-shab <shabreeshnair1998@gmail.com>
2025-01-21 22:01:39 +0530

    Scaffold CRUD routes

--- src/app.js
+++ src/app.js
@@ -4,20 +4,59 @@ const User = require("./models/user");
 
 const app = express();
 
-app.post("/signup", async (req, res) => {
-  const userObj = {
-    firstName: "Sachin",
-    lastName: "Tendulkar",
-    email: "sachin@tendulkar.com",
-    password: "ST@123",
-  };
+app.use(express.json());
 
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
   try {
-    const user = new User(userObj);
     await user.save();
     return res.send("User added successfully");
   } catch {
     return res.status(400).send("Error saving the user " + err?.message);
+  }
+});
+app.get("/user", async (req, res) => {
+  const { email } = req.body;
+  try {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    return res.status(200).json(user);
+  } catch (err) {
+    return res.status(400).send("Something went wrong");
+  }
+});
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (!users.length) {
+      return res.status(404).send("User not found");
+    }
+    return res.status(200).json(users);
+  } catch (err) {
+    return res.status(400).send("Something went wrong");
+  }
+});
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.status(200).send("User deleted successfully");
+  } catch (err) {
+    return res.status(400).send("Something went wrong");
+  }
+});
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const updatedUser = await User.findByIdAndUpdate(userId, data, {
+      returnDocument: "after",
+    });
+    res.status(200).send("User Update successfull");
+  } catch (err) {
+    res.status(400).send("Something went wrong!");
   }
 });
 
